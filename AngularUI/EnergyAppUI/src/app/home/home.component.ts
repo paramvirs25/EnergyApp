a39f32d20b281e356fa92f796e880d423c319be1
shaminder0927 <shaminder.work@gmail.com>
2018-10-16 13:50:22 +0530

    Adding Icons to tabs and accodian

--- AngularUI/EnergyAppUI/src/app/home/home.component.ts
+++ AngularUI/EnergyAppUI/src/app/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
   constructor() { }
-
-  faCoffee = faCoffee;
-
+  
   ngOnInit(): void {
     //var myOptions = {
     //  autoplay: true,
