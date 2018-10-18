161da110cdada8ab6997e479e0351c417db339ae
Shaminder Singh <shaminder87@hotmail.com>
2018-10-18 13:46:48 +0530

    Added packages for Angular-material , added basic styling -2.5hr

--- AngularUI/EnergyAppUI/src/app/app.module.ts
+++ AngularUI/EnergyAppUI/src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { AppComponent }  from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule,
+         MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule,
+         MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule,
+         MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule
+       } from '@angular/material';
+
+import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -27,38 +34,51 @@ import { LoginLayoutComponent } from './layouts/login-layout/login-layout.compon
 
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        FormsModule,
-        routing,
-        FontAwesomeModule,
-        BsDropdownModule.forRoot(),
-        TooltipModule.forRoot(),
-        ModalModule.forRoot()
-    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        HomeComponent,
-        LoginComponent,
-        RegisterComponent,
-        DashboardComponent,
-        HomeLayoutComponent,
-        LoginLayoutComponent
-    ],
-    providers: [
-        AuthGuard,
-        AlertService,
-        AuthenticationService,
-      UserService,
-      UserShared,
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-    ],
-    exports: [BsDropdownModule, TooltipModule, ModalModule],
-    bootstrap: [AppComponent]
+  exports: [MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatStepperModule,
+    MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule,
+    MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule,
+    MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule,
+    MatTreeModule
+  ]
+})
+export class AngularMaterialModule { }
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    routing,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    HomeLayoutComponent,
+    LoginLayoutComponent
+  ],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    UserShared,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule { }
+
